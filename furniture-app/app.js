6ce4a71ff438f3a82f18f52dbd635ca241e7ad04
Blair Wishom <blwishom@gmail.com>
2023-10-01 11:30:01 -0700

    styling and functionality

--- furniture-app/app.js
+++ furniture-app/app.js
@@ -1,11 +1,10 @@
 const products = [
     { id: 1, name: 'Sofa', price: 500 },
     { id: 2, name: 'Coffee Table', price: 200 },
-    { id: 3, name: 'Chair', price: 100 },
-    { id: 4, name: 'Twin Bed', price: 300 },
-    { id: 5, name: 'Full Bed', price: 400 },
-    { id: 6, name: 'Queen Bed', price: 600 },
-    { id: 7, name: 'King Bed', price: 800 },
+    { id: 3, name: 'Twin Bed', price: 300 },
+    { id: 4, name: 'Full Bed', price: 400 },
+    { id: 5, name: 'Queen Bed', price: 600 },
+    { id: 6, name: 'King Bed', price: 800 },
 ];
 
 let cart = [];
@@ -18,33 +17,53 @@ function addToCart(productId) {
     }
 }
 
+function removeItemFromCart(index) {
+    if (index >= 0 && index < cart.length) {
+        cart.splice(index, 1);
+        updateCart();
+    }
+}
+
+function emptyCart() {
+    cart = [];
+    updateCart();
+}
+
 function updateCart() {
     const cartItems = document.getElementById('cartItems');
     const cartTotal = document.getElementById('cartTotal');
     cartItems.innerHTML = '';
     let total = 0;
-    cart.forEach(item => {
+
+    cart.forEach((item, index) => {
         const cartItem = document.createElement('div');
         cartItem.textContent = `${item.name} - $${item.price}`;
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'x';
+        removeButton.addEventListener('click', () => {
+            removeItemFromCart(index);
+        });
+
+        cartItem.appendChild(removeButton);
         cartItems.appendChild(cartItem);
         total += item.price;
     });
+
+    const emptyButton = document.getElementById('emptyCart');
+    emptyButton.addEventListener('click', () => {
+        emptyCart();
+    });
+
     cartTotal.textContent = `Total: $${total.toFixed(2)}`;
 }
 
-function logout() {
-    localStorage.removeItem('loggedInUser');
-    location.href = 'login.html';
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const productButtons = document.querySelectorAll('.product-item button');
     productButtons.forEach(button => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             const productId = parseInt(button.getAttribute('data-product-id'));
             addToCart(productId);
         });
     });
-
-    updateCart();
 });
--- furniture-app/login.js
+++ furniture-app/login.js
@@ -1,6 +1,5 @@
-// Function to handle login
 function login(event) {
-    event.preventDefault(); // Prevent the form from submitting
+    event.preventDefault();
 
     const usernameInput = document.getElementById('usernameInput');
     const passwordInput = document.getElementById('passwordInput');
@@ -9,7 +8,6 @@ function login(event) {
     const username = usernameInput.value;
     const password = passwordInput.value;
 
-    // Simulate user authentication (Replace with your actual authentication logic)
     const users = [
         { username: 'user1', password: 'password1' },
         { username: 'user2', password: 'password2' },
@@ -19,14 +17,11 @@ function login(event) {
     const authenticatedUser = users.find(user => user.username === username && user.password === password);
 
     if (authenticatedUser) {
-        // Successful login
         localStorage.setItem('loggedInUser', username);
-        location.href = 'index.html'; // Redirect to the home page
+        location.href = 'index.html';
     } else {
-        // Failed login
         loginMessage.textContent = 'Invalid username or password. Please try again.';
     }
 }
 
-// Add event listener for the login form submission
 document.getElementById('loginForm').addEventListener('submit', login);
